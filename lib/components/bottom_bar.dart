9972cdbc16a0b355ad94d7b8268a07ce8253a55e
Muhammad Faiq Haidar <mfaiq.haidar86@gmail.com>
2023-05-06 22:08:54 +0800

    navigation bottom bar created

---
+++ lib/components/bottom_bar.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../pages/home_page.dart';
+import '../style/style.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  _BottomBarState createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    Text('Index 1: Business'),
+    Text('Index 2: School'),
+    Text('Index 3: School'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            activeIcon: SvgPicture.asset(
+              AppStyle.homeIcon,
+              color: AppStyle.primarySwatch,
+            ),
+            icon: SvgPicture.asset(AppStyle.homeIcon),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: SvgPicture.asset(
+              AppStyle.eventIcon,
+              color: AppStyle.primarySwatch,
+            ),
+            icon: SvgPicture.asset(AppStyle.eventIcon),
+            label: 'Schedule',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: SvgPicture.asset(
+              AppStyle.reportIcon,
+              color: AppStyle.primarySwatch,
+            ),
+            icon: SvgPicture.asset(AppStyle.reportIcon),
+            label: 'Report',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: SvgPicture.asset(
+              AppStyle.notificationsIcon,
+              color: AppStyle.primarySwatch,
+            ),
+            icon: SvgPicture.asset(AppStyle.notificationsIcon),
+            label: 'Notifications',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppStyle.primarySwatch,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
