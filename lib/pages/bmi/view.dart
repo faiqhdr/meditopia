c1de534011dab20024ae72c064cf909cc7e1a5ac
Muhammad Faiq Haidar <mfaiq.haidar86@gmail.com>
2023-06-06 21:20:33 +0800

    male female icon button

--- lib/pages/bmi/view.dart
+++ lib/pages/bmi/view.dart
@@ -24,6 +24,8 @@ class BmiCalculator extends StatelessWidget {
               children: const [
                 // Header Area.
                 Header(),
+                // Gender Area.
+                Gender(),
               ],
             ),
           ),
@@ -63,6 +65,130 @@ class Header extends StatelessWidget {
         child: SvgPicture.asset(
           AppStyle.bmiIcon,
         ),
+      ),
+    );
+  }
+}
+
+class Gender extends StatelessWidget {
+  const Gender({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 55, right: 61, bottom: 74),
+      width: double.infinity,
+      height: 111,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 63),
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(padding: EdgeInsets.zero),
+              child: Container(
+                width: 78,
+                height: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(bottom: 10),
+                      padding: const EdgeInsets.all(11),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: const Color(0xffffffff),
+                        borderRadius: BorderRadius.circular(39),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color(0x3f000000),
+                            offset: Offset(0, 4),
+                            blurRadius: 2,
+                          ),
+                        ],
+                      ),
+                      child: Center(
+                        child: SizedBox(
+                          width: 56,
+                          height: 56,
+                          child: Image.network(
+                            'https://cdn3.iconfinder.com/data/icons/avatars-flat/33/man_5-1024.png',
+                            width: 56,
+                            height: 56,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      'Male',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700,
+                        height: 1.35,
+                        letterSpacing: 0.17,
+                        color: Color(0xff758494),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(padding: EdgeInsets.zero),
+            child: SizedBox(
+              width: 78,
+              height: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    padding: const EdgeInsets.all(8),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(39),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x3f000000),
+                          offset: Offset(0, 4),
+                          blurRadius: 2,
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: SizedBox(
+                        width: 62,
+                        height: 62,
+                        child: Image.network(
+                          'https://cdn1.iconfinder.com/data/icons/website-internet/48/website_-_female_business-512.png',
+                          width: 62,
+                          height: 62,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(right: 1),
+                    child: const Text(
+                      'Female',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.w700,
+                        height: 1.35,
+                        letterSpacing: 0.17,
+                        color: Color(0xff758493),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
