38afe5b72c64aa79779edda3d8a7bbd0d54e27fe
Muhammad Faiq Haidar <mfaiq.haidar86@gmail.com>
2023-06-11 20:13:07 +0800

    firebase and flutterfire configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:meditopia/pages/account/login.dart';
-import 'package:meditopia/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensure Flutter binding is initialized
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
