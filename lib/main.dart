d7bfee6828d46f13f25b565568065c7b6c8b2332
Muhammad Faiq Haidar <mfaiq.haidar86@gmail.com>
2023-06-14 09:52:51 +0800

    firebase connection status

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
+// cimport 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:meditopia/pages/account/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Ensure Flutter binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  final List<FirebaseApp> apps = await Firebase.apps;
+
+  if (apps.isEmpty) {
+    print('Firebase is not connected.');
+  } else {
+    print('Firebase is connected.');
+  }
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
