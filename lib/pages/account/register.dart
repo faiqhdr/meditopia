fc782c2f1881c38f9ef5f648c8a328d49d5c5195
Muhammad Faiq Haidar <mfaiq.haidar86@gmail.com>
2023-06-17 22:39:48 +0800

    firestore enabled"

--- lib/pages/account/register.dart
+++ lib/pages/account/register.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meditopia/main.dart';
 import '../../style/style.dart';
 
 class RegisterPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController = TextEditingController();
 
   RegisterPage({Key? key}) : super(key: key);
 
@@ -20,65 +21,7 @@ class RegisterPage extends StatelessWidget {
   Future<void> _register(BuildContext context) async {
     final String emailAddress = emailController.text.trim();
     final String password = passwordController.text;
-
-    if (emailAddress.isEmpty && password.isEmpty) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Error'),
-            content: const Text('Please enter an email and password.'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-      return;
-    } else if (emailAddress.isEmpty) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Error'),
-            content: const Text('Please enter an email.'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-      return;
-    } else if (password.isEmpty) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Error'),
-            content: const Text('Please enter a password.'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
-      return;
-    }
+    final String name = nameController.text;
 
     try {
       final credential =
@@ -86,7 +29,19 @@ class RegisterPage extends StatelessWidget {
         email: emailAddress,
         password: password,
       );
+
+      // Store user information in Firestore
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(credential.user!.uid)
+          .set({
+        'name': name,
+        'email': emailAddress,
+        'password': password,
+      });
+
       // Registration successful, do something
+      _navigateToSignIn(context);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         showDialog(
@@ -126,23 +81,7 @@ class RegisterPage extends StatelessWidget {
         );
       }
     } catch (e) {
-      showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Error'),
-            content: const Text('An error occurred. Please try again.'),
-            actions: <Widget>[
-              TextButton(
-                child: const Text('OK'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-            ],
-          );
-        },
-      );
+      print(e);
     }
   }
 
@@ -150,8 +89,7 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding:
-            const EdgeInsets.only(right: 25, left: 25, top: 153, bottom: 0),
+        padding: const EdgeInsets.only(right: 25, left: 25, top: 93, bottom: 0),
         width: double.infinity,
         decoration: const BoxDecoration(
           color: Color(0xff3671a4),
@@ -210,6 +148,29 @@ class RegisterPage extends StatelessWidget {
                       obscureText: true,
                       decoration: const InputDecoration(
                         hintText: 'Password',
+                        border: InputBorder.none,
+                      ),
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 0.18,
+                        color: Color(0xff0e1012),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.only(
+                        left: 25, right: 25, top: 1, bottom: 1),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: TextFormField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Name',
                         border: InputBorder.none,
                       ),
                       style: const TextStyle(
