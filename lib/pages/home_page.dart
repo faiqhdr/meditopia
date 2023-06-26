104e68df72d3d78c43791fe0e1e3136049dd8b51
Muhammad Faiq Haidar <mfaiq.haidar86@gmail.com>
2023-06-26 22:47:22 +0800

    user info home page updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_svg/svg.dart';
 import '../data/data.dart';
 import '../size_config.dart';
@@ -46,59 +48,89 @@ class UserInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: EdgeInsets.only(
-        top: SizeConfig.blockSizeVertical! * 3,
-        bottom: 16.0,
-      ),
-      title: const Padding(
-        padding: EdgeInsets.only(
-          bottom: 7,
-          top: 7,
-        ),
-        child: Text("👋 Hello!"),
-      ),
-      subtitle: Padding(
-        padding: EdgeInsets.only(
-          bottom: SizeConfig.blockSizeVertical! * 0,
-        ),
-        child: Text(
-          "Master Oogway",
-          style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                fontWeight: FontWeight.w700,
-              ),
-        ),
-      ),
-      trailing: Container(
-        height: 48,
-        width: 48,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(AppStyle.profile),
-            fit: BoxFit.cover,
-            repeat: ImageRepeat.repeat,
+    User? user = FirebaseAuth.instance.currentUser;
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: firestore.collection('users').doc(user!.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Error fetching user data'),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data!.data() == null) {
+          return const Center(
+            child: Text('No user data found'),
+          );
+        }
+
+        var userData = snapshot.data!.data() as Map<String, dynamic>;
+        var userName = userData['name'] ?? 'Unknown';
+
+        return ListTile(
+          contentPadding: EdgeInsets.only(
+            top: SizeConfig.blockSizeVertical! * 3,
+            bottom: 16.0,
           ),
-          borderRadius: BorderRadius.all(Radius.circular(18.0)),
-        ),
-        child: Stack(
-          alignment: Alignment.topRight,
-          children: [
-            Container(
-              width: 18.0,
-              height: 18.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: AppStyle.primarySwatch,
-                border: Border.all(
-                  color: Colors.white,
-                  width: 3.0,
-                  style: BorderStyle.solid,
-                ),
-              ),
+          title: const Padding(
+            padding: EdgeInsets.only(
+              bottom: 7,
+              top: 7,
             ),
-          ],
-        ),
-      ),
+            child: Text("👋 Hello!"),
+          ),
+          subtitle: Padding(
+            padding: EdgeInsets.only(
+              bottom: SizeConfig.blockSizeVertical! * 0,
+            ),
+            child: Text(
+              userName,
+              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                    fontWeight: FontWeight.w700,
+                  ),
+            ),
+          ),
+          trailing: Container(
+            height: 48,
+            width: 48,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppStyle.profile),
+                fit: BoxFit.cover,
+                repeat: ImageRepeat.repeat,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(18.0)),
+            ),
+            child: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Container(
+                  width: 18.0,
+                  height: 18.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: AppStyle.primarySwatch,
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 3.0,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
